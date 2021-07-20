5ca6afc746fee9367a3ff15bf68b1b88e8eb410c
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2021-07-20 19:50:09 +0000

    feat: add should_confirm flag to my_details.py

--- my_details_TEMPLATE.py
+++ my_details_TEMPLATE.py
@@ -1,5 +1,10 @@
+# NOTE: Set this with your publicly-known handle for labelling purposes
 handle = ''
 
+# NOTE: Set this to false if you only use your wallets for DAO purposes and all of your txs are reimbursable
+should_confirm = True 
+
+# NOTE: Fill in this list with the address(es) you use for your work for the DAO
 my_addresses = [
     "",
 ]
