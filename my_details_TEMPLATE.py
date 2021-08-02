4bd69e39bced6ac8b47d3e84e161e8cee2eaed62
Poolpi Tako <poolpitako@protonmail.com>
2021-08-02 08:26:41 -0500

    poolpi aug 02 2021

--- my_details_TEMPLATE.py
+++ my_details_TEMPLATE.py
@@ -1,8 +1,8 @@
 # NOTE: Set this with your publicly-known handle for labelling purposes
 handle = ''
 
-# NOTE: Set this to false if you only use your wallets for DAO purposes and all of your txs are reimbursable
-should_confirm = True 
+# NOTE: Set this to true if you only use your wallets for DAO purposes and all of your txs are reimbursable
+skip_confirm = False
 
 # NOTE: Fill in this list with the address(es) you use for your work for the DAO
 my_addresses = [
