aae3a8d9e5da08721e04859e333ec9d014cec2c7
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2021-08-18 18:40:01 +0000

    chore: rename should_confirm to skip_confirm

--- my_details_TEMPLATE.py
+++ my_details_TEMPLATE.py
@@ -2,7 +2,7 @@
 handle = ''
 
 # NOTE: Set this to false if you only use your wallets for DAO purposes and all of your txs are reimbursable
-should_confirm = True 
+skip_confirm = True 
 
 # NOTE: Fill in this list with the address(es) you use for your work for the DAO
 my_addresses = [
